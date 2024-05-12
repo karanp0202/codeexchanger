a7a4bea1aca56583d1ab13cf0cf554ae97116eba
Karan <karanp0202@gmail.com>
2024-05-12 18:16:28 +0530

    pages replaced with docs for github static hosting

---
+++ docs/script.js
@@ -0,0 +1,57 @@
+const ws = new WebSocket('wss://codexchanger.azurewebsites.net'); // Match server's port
+
+ws.onopen = () => {
+    if (window.location.hash)
+    {
+        codeid.value = window.location.hash.slice(1)
+        codeidchange()
+    }
+};
+
+// ANY MESSAGE FROM SOCKET
+ws.onmessage = (event) => {
+    const message = JSON.parse(event.data)
+    switch (message.type) {
+
+        case 'codechange':
+            console.log(message.changedcode)
+            code.value = message.changedcode
+            document.getElementById("clientCount").innerText = message.nclients
+            break;
+
+        case 'issue':event
+            break
+        default:
+            break;
+    }
+};
+
+// ON ISSUE OR ANY OTHER THINGS
+function codeidchange(){
+    ws.send(JSON.stringify({
+        type: "codeidchange",
+        codeid: codeid.value,
+        previous: previous_codeid
+    }))
+    previous_codeid = codeid.value
+    window.location.hash = '#' + codeid.value
+}
+
+let previous_codeid = undefined
+
+function codechange(){
+    ws.send(JSON.stringify({
+        type: "codechange",
+        codeid: codeid.value,
+        code: code.value,
+    }))
+}
+
+function copytoclipboard()
+{
+    navigator.clipboard.writeText(window.location)
+    copy_link.innerText = "copied"
+    setTimeout(() => {
+        copy_link.innerText = "copy link"
+    }, 2000);
+}
